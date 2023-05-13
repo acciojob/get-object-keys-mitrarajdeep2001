4090b889767a4033c2b33f238d3737f5e2ec5be9
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-05-13 10:26:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+	name: 'Rahul',
+	class: 12,
+	section:'c'
+}
+
+Object.prototype.getKeys = function () {
+	return Object.keys(this);
+}
+
+console.log(student.getKeys())
